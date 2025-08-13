16fa47e258ce59ec5758dcef922d60bec2f9401e
Salavat <salavat2602@mail.ru>
2025-08-13 22:29:45 +0500

    tst

--- marvel_starter/src/components/app/App.jsx
+++ marvel_starter/src/components/app/App.jsx
@@ -23,7 +23,7 @@ const App = () =>  {
         <div className="app">
             <AppHeader/>
              <main>
-                {/* <ErrorBoundary>
+                <ErrorBoundary>
                     <RandomChar/>
                 </ErrorBoundary>
                 <div className="char__content">
@@ -34,7 +34,7 @@ const App = () =>  {
                         <CharInfo charId={selectedChar}/>
                     </ErrorBoundary>
                 </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/> */}
+                <img className="bg-decoration" src={decoration} alt="vision"/>
                 <AppBanner/>
                 <ComicsList/>
             </main>
