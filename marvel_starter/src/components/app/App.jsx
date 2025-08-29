7a444bc67b3f44b44b8b55e925b8ce85ce1eb650
Salavat <salavat2602@mail.ru>
2025-08-29 20:46:16 +0500

    route 6v

--- marvel_starter/src/components/app/App.jsx
+++ marvel_starter/src/components/app/App.jsx
@@ -1,32 +1,24 @@
-
-import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import AppHeader from "../appHeader/AppHeader";
-import {MainPage, ComicsPage} from '../pages';
+import { MainPage, ComicsPage } from "../pages";
 
+const App = () => {
+  return (
+    <Router>
+      <div className="app">
+        <AppHeader />
+        <main>
+          <Routes>
 
+            <Route  path="/" element={<MainPage />} />
+            <Route  path="/comics" element={<ComicsPage />} />
 
-const App = () =>  {
-
-   
-        return (
-       <Router>
-         <div className="app">
-            <AppHeader/>
-             <main>
-                <Switch>
-                    <Route exact path= "/">
-                    <MainPage/>
-                </Route>
-               <Route exact path="/comics">
-                 <ComicsPage/>
-               </Route>
-                </Switch>
-            </main>
-        </div>
-       </Router>
-    )
- 
-}
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
--- marvel_starter/src/components/appHeader/AppHeader.jsx
+++ marvel_starter/src/components/appHeader/AppHeader.jsx
@@ -1,24 +1,44 @@
-import { Link, NavLink } from 'react-router-dom/cjs/react-router-dom.min';
+import { Link, NavLink } from "react-router-dom";
 
-import './appHeader.scss';
+import "./appHeader.scss";
 
 const AppHeader = () => {
-    return (
-        <header className="app__header">
-            <h1 className="app__title">
-                <Link to="/">
-                    <span>Marvel</span> information portal
-                </Link>
-            </h1>
-            <nav className="app__menu">
-                <ul>
-                    <li><NavLink  exact activeStyle={{'color' : '#9f0013'}} to="/">Characters</NavLink></li>
-                    /
-                    <li><NavLink exact activeStyle={{'color' : '#9f0013'}} to="/comics">Comics</NavLink></li>
-                </ul>
-            </nav>
-        </header>
-    )
-}
+  return (
+    <header className="app__header">
+      <h1 className="app__title">
+        <Link to="/">
+          <span>Marvel</span> information portal
+        </Link>
+      </h1>
+      <nav className="app__menu">
+        <ul>
+          <li>
+            <NavLink
+              end
+              style={({ isActive }) => ({
+                color: isActive ? "#9f0013" : "inherit",
+              })}
+              to="/"
+            >
+              Characters
+            </NavLink>
+          </li>
+          /
+          <li>
+            <NavLink
+              end
+              style={({ isActive }) => ({
+                color: isActive ? "#9f0013" : "inherit",
+              })}
+              to="/comics"
+            >
+              Comics
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default AppHeader;
